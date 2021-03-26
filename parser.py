7aee01cb42ac3f49aeddc03459ff2b3b6f7deb16
jean <jean>
2021-03-26 13:02:09 +0100

    generating sublinks

--- parser.py
+++ parser.py
@@ -43,7 +43,7 @@ class DividePages(object):
         #init le parseur regex
         #find les lignes avec name et #T
         res = re.findall(regex_toc_line, self.file_content)
-        toc_content = re.search(regex_toc_content, self.file_content).group(1)
+        self.toc_content = re.search(regex_toc_content, self.file_content).group(1)
         
         for item in res:
             #creation des mdchunk avec page_name
@@ -52,15 +52,32 @@ class DividePages(object):
             content = re.search(regex_content, str(self.file_content), re.MULTILINE)
             if content is not None:
                 self.md_chunk_list.append(MdChunk(item[0], item[1], content.group(1)))
+    
+    
 
     def rewrite_links(self):
-        toc_lines = re.findall(regex_toc_line, self.file_content)
-        for item in toc_lines:
-            uri = self.args[3]
-            link = uri + item[0] + '/ '
-            test_after = re.sub(regex_toc_link, link, self.toc_content)
-        
-
+        regex_rewrite = "#T.*-"
+        self.toc_content = re.sub(regex_rewrite, self.args[3], self.toc_content)
+        toc_lines = self.toc_content.split("\n")
+        link = ""
+        new_toc_content = ""
+        for line in toc_lines:
+            #print(line)
+            res = re.search("(https.*) ", line)
+            #enters if if finds https
+            if res is not None:
+                link = res.group(1)
+            #did not find https
+            else:
+                pos = re.search("#[A-Z]", line)
+                if pos is not None:
+                    pos_int = pos.start()
+                    if pos_int > 0:
+                        line = line[ : pos_int] + link + line[pos_int : ]
+            new_toc_content += line + '\n'
+        self.toc_content = new_toc_content
+        print(self.toc_content)
+    
     def write_files(self):
         directory = '{}\\'.format(self.args[2])
         home_file_name = directory + "Home.md"
